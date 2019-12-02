28b649ba95412743aeb81dec43381864a1d0773c
Lukas Jezny <ljezny@gmail.com>
2019-12-02 20:45:56 +0100

    Commiting database

--- OperativeLeaseCalc/Model/AppModel.swift
+++ OperativeLeaseCalc/Model/AppModel.swift
@@ -51,6 +51,7 @@ class AppModel:NSObject, ObservableObject {
                     if !b {
                         self?.notifications = false
                     }
+                    PersistentStorageManager.shared.saveContext()
                 }
             }
         }
@@ -65,6 +66,7 @@ class AppModel:NSObject, ObservableObject {
             if v {
                 let _ = OBD2Manager.shared
             }
+            PersistentStorageManager.shared.saveContext()
         }
     }
     
