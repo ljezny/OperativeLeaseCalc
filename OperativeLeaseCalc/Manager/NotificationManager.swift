9b412103402b96cb023a273b7659cf38aab7c636
Lukas Jezny <ljezny@gmail.com>
2020-01-03 20:35:05 +0100

    Some notification minor fixes

--- OperativeLeaseCalc/Manager/NotificationManager.swift
+++ OperativeLeaseCalc/Manager/NotificationManager.swift
@@ -12,7 +12,7 @@ class NotificationManager {
     static let shared = NotificationManager()
     
     func requestPermission(completion: @escaping (Bool) -> ()) {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound]) { (b,error ) in
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) { (b,error ) in
             completion(b)
         }
     }
@@ -26,14 +26,13 @@ class NotificationManager {
     }
     
     func notifyDisconnection() {
-        notify(title: NSLocalizedString("notification.title", comment: ""), body:  NSLocalizedString("notification.disconnet.body", comment: ""), id: "ConnectionState")
+        notify(title: NSLocalizedString("notification.title", comment: ""), body:  NSLocalizedString("notification.disconnect.body", comment: ""), id: "ConnectionState")
     }
     
     func notify(title: String, body: String, id: String) {
         let content = UNMutableNotificationContent()
         content.title = title
         content.body = body
-        content.sound = UNNotificationSound.default
         UNUserNotificationCenter.current().add(UNNotificationRequest.init(identifier: id, content:content, trigger: nil)) { (error) in
             
         }
